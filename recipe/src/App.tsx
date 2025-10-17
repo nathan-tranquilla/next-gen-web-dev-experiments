0234d70ed68de88a39630ef4427caac7d3ed91ef
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-10-17 10:01:48 -0300

    integrate basic recipe form without onAddRecipe prop

--- recipe/src/App.tsx
+++ recipe/src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 import React, { useState } from 'react';
-import RecipeForm from './components/RecipeForm.tsx';
+// import RecipeForm from './components/RecipeForm.tsx';
+import { make as RecipeForm } from './components/RecipeForm.res.mjs' 
 import RecipeList from './components/RecipeList.tsx';
 import { Recipe } from './types.ts';
 
@@ -16,10 +17,10 @@ function App() {
       <div className="max-w-4xl mx-auto px-4">
         <h1 className="text-4xl font-bold text-gray-900 text-center mb-8">Recipe Planner</h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <RecipeForm onAddRecipe={addRecipe} />
+          {/* <RecipeForm onAddRecipe={addRecipe} /> */}
+          <RecipeForm />
           <RecipeList recipes={recipes} />
         </div>
-        {/* TODO: Add Meal Planner Calendar component here */}
       </div>
     </div>
   );
